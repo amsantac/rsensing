26625de209e3e69a51546415ef275f7e6fcd78ac
amsd2013@gmail.com <amsd2013@gmail.com@3ddcebff-eea8-4cec-466b-101479f2c20e>
2011-07-20 23:07:01 +0000

    2do cambio de sincroniz

--- trunk/0.0-1/MorePointsNet.r
+++ trunk/0.0-1/MorePointsNet.r
@@ -1,6 +1,7 @@
 netDesign <- function(inpol, n, type){
 #cambio de sincronizacion
 require(maptools)
+#2o cambio de syncr
 if(type=="hexagonal") {
 pts.est <- spsample(inpol, 1.2*n, type)
 while (summary(pts.est)[5]$npoints != n) pts.est <- spsample(inpol, 1.2*n, type)
