b8d2669c6ce20fae1e30a76d41444ea3ad6e982a
amsd2013@gmail.com <amsd2013@gmail.com@3ddcebff-eea8-4cec-466b-101479f2c20e>
2011-07-21 00:38:35 +0000

    se cambia una palabra y se adiciona una frase

--- trunk/0.0-1/MorePointsNet.r
+++ trunk/0.0-1/MorePointsNet.r
@@ -1,8 +1,9 @@
 netDesign <- function(inpol, n, type){
 #cambio de sincronizacion
 require(maptools)
-#2o cambio de syncr
+#2o cambio de syncrOniz
 if(type=="hexagonal") {
+#cambio para merge
 pts.est <- spsample(inpol, 1.2*n, type)
 while (summary(pts.est)[5]$npoints != n) pts.est <- spsample(inpol, 1.2*n, type)
 } else {
