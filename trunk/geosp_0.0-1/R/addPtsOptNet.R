27c0bd747fa7fd80baaf031454cabea3cc9674e3
amsd2013@gmail.com <amsd2013@gmail.com@3ddcebff-eea8-4cec-466b-101479f2c20e>
2011-08-04 21:15:16 +0000

    upload code red puntos adicionales, el mismo con grafico, y los Rd

---
+++ trunk/geosp_0.0-1/R/addPtsOptNet.R
@@ -0,0 +1,19 @@
+addPtsOptNet <- function(formula, locations, spDatF, fitmodel, n, popSize, generations, xmin, ymin, xmax, ymax, ...){
+	evaluate <- function(string=c()) {
+		returnVal = NA;
+		pts2 <- as.data.frame(matrix(0, ncol=2, nrow=n))
+		for (i in 1:n){
+			pts2[i,1] <- round(string[i], 1)
+		}
+		for (j in 1:n){
+			pts2[j,2] <- round(string[n + j], 1)
+		}
+		names(pts2) <- c("x", "y")
+		coordinates(pts2) = c("x", "y")
+		interp <- krige(formula, locations, data=spDatF, newdata=pts2, model=fitmodel, ...)
+		returnVal <- sum(sqrt(interp[["var1.var"]]))/n
+		returnVal
+	}
+	results <- rbga(as.matrix(c(rep(xmin,n), rep(ymin, n))), as.matrix(c(rep(xmax,n), rep(ymax, n))), popSize=popSize, evalFunc=evaluate, verbose=TRUE, iters=generations, ...)
+}
+
