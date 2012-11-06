4e0bfb58f3c0947d5a4625ee56fd390229d32adb
amsantac@gmail.com <amsantac@gmail.com@3ddcebff-eea8-4cec-466b-101479f2c20e>
2012-11-06 20:14:09 +0000

    Deleted unnecessary lines in Kallocation.R

--- rAA_0.0-1/R/Kallocation.R
+++ rAA_0.0-1/R/Kallocation.R
@@ -15,15 +15,6 @@ min2prop <- apply(rbind(ref1prop, ref2prop), 2, min)
 
 prodprop <- ref1prop*ref2prop
 
-persist <- diag(res1)/sumtot
-
-loss <- ref1prop - persist
-
-gain <- ref2prop - persist
-
-gainIntens <- gain/ref2prop
-
-
 AIpQIn <- sum(min1prop)
 AIpQIm <- sum(min2prop)
 AIpQIp <- 1
@@ -36,27 +27,5 @@ AInQIp <- ref2prop %*% ref2prop
 
 Kallocation <- (AImQIm - AInQIm)/(AIpQIm - AInQIm)
 
-AImQIn <- AInQIn + Kallocation * (AIpQIn - AInQIn)
-AImQIp <- AInQIp + Kallocation * (AIpQIp - AInQIp)
-
-Kno <- (AImQIm - AInQIn)/(AIpQIp - AInQIn)
-Kquantity <- (AImQIm - AImQIn)/(AImQIp - AImQIn)
-Khisto <- Khisto <- (AIpQIm - AInQIm)/(AIpQIp - AInQIm)
-Kstandard <- (AImQIm - AInQIm)/(AIpQIp - AInQIm)
-
-chanceAgreement <- 100 * min(AInQIn, AImQIm, AInQIm)
-quantityAgreement <- ifelse(chanceAgreement==AInQIn, 100 * min(AInQIm - AInQIn, AImQIm - AInQIn), 0)
-allocationAgreement <- 100 * max(AImQIm - AInQIm, 0)
-allocationDisagreement <- 100 * (AIpQIm - AImQIm)
-quantityDisagreement <- 100 * (AIpQIp - AIpQIm)
-pierceSkillScoreBooleanCase <- 0
-figureMeritBooleanCase <- 0
-
-AIQImat <- matrix(c(AIpQIn, AImQIn, AInQIn, AIpQIm, AImQIm, AInQIm, AIpQIp, AImQIp, AInQIp), 3, 3)
-rownames(AIQImat) <- c("AIperfect", "AImedium", "AIno")
-colnames(AIQImat) <- c("QIno", "QImedium", "QIperfect")
-
-res2 <- list(AIQImat = AIQImat, Kno = Kno, Kallocation = Kallocation, Kquantity = Kquantity[1], Khisto = Khisto, Kstandard = Kstandard, chanceAgreement = chanceAgreement, quantityAgreement = quantityAgreement, allocationAgreement = allocationAgreement, allocationDisagreement = allocationDisagreement, quantityDisagreement = quantityDisagreement, pierceSkillScoreBooleanCase = pierceSkillScoreBooleanCase, figureMeritBooleanCase = figureMeritBooleanCase)
-
-return(res2)
+return(Kallocation)
 }
